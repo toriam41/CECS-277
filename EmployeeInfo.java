40fd0c34b210514b9ff799caa369cdbe49d35a95
Tori McDonald <31860131+toriam41@users.noreply.github.com>
2021-07-26 16:03:51 -0400

    Payroll System Project
    Employee information interface construction

---
+++ EmployeeInfo.java
@@ -0,0 +1,14 @@
+/**
+ * Tori McDonald
+ * CECS 277-15
+ * Lab 2 - Payroll System
+ * 3/2/2020
+ */
+
+package lab2;
+
+public interface EmployeeInfo {
+
+	double EMPLOYEE_MONTHLY_SALARY = 5000.00;
+	int STAFF_MONTHLY_HOURS_WORKED = 150;
+}
