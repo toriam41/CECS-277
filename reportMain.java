a92a11fa31523c278bcf7b269211152930f856a8
Tori McDonald <31860131+toriam41@users.noreply.github.com>
2021-07-26 16:10:01 -0400

    Report Card Project
    Main program

---
+++ reportMain.java
@@ -0,0 +1,18 @@
+/**
+ * Tori McDonald
+ * CECS 277-15
+ * Lab 3 - Report Card
+ * 3/2/2020
+ */
+
+package lab3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ReportCard card = new ReportCard();
+		
+		card.display(card.schedule);
+	}
+
+}
