812c9cf927e55d394b8bb5be5fa3fbf973572184
Tori McDonald <31860131+toriam41@users.noreply.github.com>
2021-07-26 16:43:48 -0400

    Adapter Pattern Project
    Full name interface construction

---
+++ FullNameInterface.java
@@ -0,0 +1,10 @@
+package lab8.adapterpattern;
+
+public interface FullNameInterface {
+	
+	public void setFirstName(String f);
+	public void setLastName(String l);
+	public String getFirstName();
+	public String getLastName();
+	
+}
