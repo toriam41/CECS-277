0f8d1933c2ac62de22fc166c14ba44a62b73bd53
Tori McDonald <31860131+toriam41@users.noreply.github.com>
2021-07-26 22:52:48 -0400

    State Design Pattern Project
    Class construction

---
+++ HappyRobot.java
@@ -0,0 +1,11 @@
+package lab8.statepattern;
+
+public class HappyRobot implements Feeling {
+
+	@Override
+	public void doAction() {
+		
+		System.out.println("Robo is talking and cooking :)");
+	}
+
+}
